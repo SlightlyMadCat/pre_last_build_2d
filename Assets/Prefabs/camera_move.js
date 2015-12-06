2a648c31b612dd0fa2843b8a77f44fd188153296
SlightlyMadCat <ilyhakovalenko@gmail.com>
2015-12-06 15:21:49 +0200

    linear_new

--- Assets/Prefabs/camera_move.js
+++ Assets/Prefabs/camera_move.js
@@ -5,3 +5,22 @@ function Update () {
     speed = GameObject.Find("plane42").GetComponent(diss_2).speed;
     this.transform.position.x+=speed;
 }
+
+function Start () {
+    print(Screen.height);
+    print(Screen.width);
+
+    var scale : float;
+    scale  = Screen.width/349f;
+
+    GameObject.Find("variometr").transform.localScale.x=scale;
+    GameObject.Find("variometr").transform.localScale.y=scale;
+    GameObject.Find("variometr").transform.localScale.z=scale;
+
+    GameObject.Find("Aviahor").transform.localScale.x=scale;
+    GameObject.Find("Aviahor").transform.localScale.y=scale;
+    GameObject.Find("Aviahor").transform.localScale.z=scale;
+
+    var m = GameObject.Find("variometr").transform.localScale;
+    print(m);
+}
