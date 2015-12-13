931458c4e0523b7902a23e40645a49af8a4db2bc
SlightlyMadCat <ilyhakovalenko@gmail.com>
2015-12-13 11:33:23 +0200

    menu_commit

--- Assets/massiv_cubov.js
+++ Assets/massiv_cubov.js
@@ -11,9 +11,10 @@ var dh = 0;
 var base_h = 0;
 var gameOver = false;
 var n = 0;
+var hPlane = 0;
 
 function Start() {      //ГЕНЕРАТОР ОБЛАКОВ
-    var rand:float;
+    /*var rand:float;
 
     for (i=0;i<mas.length;i++){
         rand = Random.Range(-15.5, 23.6);
@@ -21,7 +22,7 @@ function Start() {      //ГЕНЕРАТОР ОБЛАКОВ
         mas[i].transform.position.y = rand;
     }
 
-    i=0;
+    i=0;*/
 }
 
 function Update () { 
@@ -29,21 +30,22 @@ function Update () {
     if (i < mas.length) {
         distX = mas[i].transform.position.x - transform.position.x; // расстояние по горизонтали
         distY = mas[i].transform.position.y - transform.position.y; //расстояние по вертикали
-        var hPlane = GameObject.Find("plane42").GetComponent(diss_2).hPlane;
+        hPlane = GameObject.Find("plane42").GetComponent(diss_2).hPlane;
 
         new_height = base_h+mas[i].transform.position.y*120;
-        print(new_height);
+        //print(new_height);
         dist = distX*200;
     }
 
     stolk = GameObject.Find("plane42").GetComponent(Trigger).stolk;
 
     if (distX <=0) {
-        i++;
-        if (stolk == false && i < mas.length) {
-            score +=10;
+        if (i < mas.length){
+            i++;
+            if (i+1 < mas.length){
+                mas[i].transform.position.y = (hPlane - base_h)/120;
+            }
         }
-        //print(i);
     }
     score = GameObject.Find("cursor").GetComponent(Rotate).score;
 
@@ -54,9 +56,10 @@ function Update () {
 }
 
 function OnGUI() {
-    GUI.Box(new Rect(Screen.width/6.5,40,150,30), "Score: "+Mathf.Round(score * 100)/100);
+    GUI.Box(new Rect(Screen.width/5,Screen.height/20,150,30), "Score: "+Mathf.Round(score * 100)/100);
 
     if (gameOver == true){
-        GUI.Box( new Rect(Screen.width/2 - 200,Screen.height/2 - 160,400,300), "You get "+score+" points of "+GameObject.Find("cursor").GetComponent(Rotate).count_clouds+". "+n+" barrier(s) touched.");
+        var style = GameObject.Find("cursor").GetComponent(Rotate).style;
+        GUI.Box( new Rect(Screen.width/2 - 300,Screen.height/2 - 160,400,300), "You get "+score+" points of 100. "/*GameObject.Find("cursor").GetComponent(Rotate).count_clouds*/+n+" barrier(s) touched.",style);
     }
 }
